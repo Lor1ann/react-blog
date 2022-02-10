0b90831fc49c073ae0404a6a6f6d589f4a73f614
Eugene <hurakane122@gmail.com>
2022-02-10 16:34:19 +0300

    serv

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("build"));
 app.use(
   fileUpload({
     createParentPath: true,
--- src/axios/index.js
+++ src/axios/index.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:5656",
+  baseURL: "/",
+  headers: {
+    Authorization: window.localStorage.getItem("token"),
+  },
 });
