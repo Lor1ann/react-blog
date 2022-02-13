bb6fe5d9fe0bab64c6be5839da84a7a0bf73d49f
Eugene <hurakane122@gmail.com>
2022-02-13 12:42:38 +0300

    fix serv

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:5656",
   headers: {
     Authorization: window.localStorage.getItem("token"),
   },
